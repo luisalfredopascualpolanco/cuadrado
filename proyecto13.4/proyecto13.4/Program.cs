0861a75df4c8b07ec49c3e1f994bfca8de2bbf95
luisalfredopascualpolanco <61303559+luisalfredopascualpolanco@users.noreply.github.com>
2022-01-06 11:44:59 -0800

    version1.0

---
+++ proyecto13.4/proyecto13.4/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proyecto13._4
+{
+    class Program
+    {
+        public class Cuadrados
+        {
+            private float lado { get; set; }
+            
+            public void Cargar()
+            {
+                Console.Write("Dame el lado del cuadrado: ");
+                lado = float.Parse(Console.ReadLine());
+            }
+
+            public void Perimetro()
+            {
+                float perimetro = lado * 4;
+                Console.WriteLine("Perimetro es "+perimetro);
+            }
+
+            public void Superficie()
+            {
+                float superficie = lado * lado;
+                Console.WriteLine("Superficie es " + superficie);
+            }
+        }
+        static void Main(string[] args)
+        {
+            try
+            {
+                Cuadrados cuadrado = new Cuadrados();
+                cuadrado.Cargar();
+                cuadrado.Perimetro();
+                cuadrado.Superficie();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Ha introducido un dato invalido.");                
+            }
+            Console.ReadKey();
+        }
+    }
+}
